c63e1a541e985c5f9f49b4f24da55c71abfd2300
Alok Prateek <alok9868@gmail.com>
2018-03-15 05:32:04 +0530

    long update

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -18,8 +18,8 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 // Create the email and send the message
 $to = 'events@iecinnovision.com'; 
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: events@iecinnovision.com\n"; 
+$email_body = "Someone wanted to know something special.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: contactus@iecinnovision.com\n"; 
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
